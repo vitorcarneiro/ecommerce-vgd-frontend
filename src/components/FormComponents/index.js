90f6544775ccd2e0193b9b7e31395f786b8ddabf
pedro <pedrohperoni@gmail.com>
2022-02-09 11:24:28 -0300

    feat: add routes

---
+++ src/components/FormComponents/index.js
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import Container from "./Container";
+import Form from "./Form";
+import Input from "./Input";
+import StyledLink from "./StyledLink";
+
+export { Container, Form, Input, Button, StyledLink };
